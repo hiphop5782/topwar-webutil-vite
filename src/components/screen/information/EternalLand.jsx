048ee384f153fb4b02d1f1516ef67eec082202b1
hiphop5782 <zdlsz@hanmail.net>
2025-05-10 10:52:52 +0900

    1구역 좌표 및 설명 추가

--- src/components/screen/information/EternalLand.jsx
+++ src/components/screen/information/EternalLand.jsx
@@ -1,7 +1,7 @@
 import ELmap from "@src/assets/images/el-map.jpg";
 import Buildings from "@src/assets/json/el/buildings.json";
 import { throttle } from "lodash";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import "./EternalLand.css";
 
@@ -26,6 +26,16 @@ export default function EternalLand() {
         return ()=>window.removeEventListener("resize", updateSize);
     }, [map]);
     
+    const calculateTranslate = useCallback((building)=>{
+        let x = 15, y = 15;
+        if(building.x > 70) x = -115; 
+        else if(building.x > 30) x = -50;
+
+        if(building.y > 70) y = -115;
+        else if(building.y > 30) y = -50;
+
+        return `translate(${x}%, ${y}%)`;
+    }, []);
 
     return (<>
         <h1>영원의 땅</h1>
@@ -54,9 +64,42 @@ export default function EternalLand() {
                 {
                     top:`${hoverObject.y + 0.5}%`,
                     left:`${hoverObject.x + 0.5}%`,
+                    width: 150,
+                    transform: `${calculateTranslate(hoverObject)}`
                 }
             }>
-                <div className="fs-6">{hoverObject.name}</div>
+                <div className="fs-6 text-nowrap">{hoverObject.name}</div>
+                <hr/>
+                <div className="row text-nowrap">
+                    <div className="col-5 text-start">
+                        <span className="fw-bold text-primary me-1">1 분</span>당 
+                    </div>
+                    <div className="col-7 text-end">
+                        <span className="fw-bold text-danger">
+                            {hoverObject.point.toLocaleString()}
+                        </span> 점  
+                    </div>
+                </div>
+                <div className="row text-nowrap">
+                    <div className="col-5 text-start">
+                        <span className="fw-bold text-primary me-1">1 시간</span>당 
+                    </div>
+                    <div className="col-7 text-end">
+                        <span className="fw-bold text-danger">
+                            {(hoverObject.point * 60).toLocaleString()}
+                        </span> 점  
+                    </div>
+                </div>
+                <div className="row text-nowrap">
+                    <div className="col-5 text-start">
+                        <span className="fw-bold text-primary me-1">1 일</span>당 
+                    </div>
+                    <div className="col-7 text-end">
+                        <span className="fw-bold text-danger">
+                            {(hoverObject.point * 60 * 24).toLocaleString()}
+                        </span> 점  
+                    </div>
+                </div>
             </div>
             )}
         </div>
