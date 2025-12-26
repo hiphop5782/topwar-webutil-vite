5f963d6d0ba3cc50b4b835a8488f3bcc9a9e00be
hiphop5782 <zdlsz@hanmail.net>
2025-12-27 00:39:50 +0900

    시간 표시 데이터 오류 수정

--- src/components/screen/information/ServerAnalyzer.jsx
+++ src/components/screen/information/ServerAnalyzer.jsx
@@ -356,8 +356,7 @@ export default function ServerAnalyzer() {
                 {listData.map(server => (
                     <div className="col-lg-6 mb-4 border p-4" key={server.number}>
                         <h3 className="mb-4 fw-bold">{server.number} 서버</h3>
-                        <div>{server.time}</div>
-                        <div>({dayjs(server.time).format("YYYY년 M월 D일 H시")} 기준)</div>
+                        <div>({dayjs(server.data.time).format("YYYY년 M월 D일 H시")} 기준)</div>
 
                         {cutoffIsDecimal && (
                             <div className="text-danger fw-bold mb-2">{cutoff}M 이상 유저 수 : {getCutoffCount(server)}명</div>
