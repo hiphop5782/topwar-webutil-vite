62591622a0168d171ac0ff71bd2715c24fd5bac6
hiphop5782 <zdlsz@hanmail.net>
2026-02-10 23:20:13 +0900

    동맹 데이터 국제화 및 출력 순서 조정

--- src/components/screen/information/server/TopwarAllianceDataViewer.jsx
+++ src/components/screen/information/server/TopwarAllianceDataViewer.jsx
@@ -2,9 +2,11 @@ import AllianceListData from "@src/assets/json/power/allianceData.json";
 import { useMemo, useState } from "react";
 import { Virtuoso } from "react-virtuoso";
 import "./TopwarData.css";
+import { useTranslation } from "react-i18next";
 
 export default function TopwarAllianceDataViewer() {
     const [searchTerm, setSearchTerm] = useState("");
+    const { t } = useTranslation("viewer");
 
     // 1. 원본 데이터 정렬 (기존 유지, 렌더링 시 재계산 방지)
     const sortedAlliances = useMemo(() => {
@@ -25,13 +27,13 @@ export default function TopwarAllianceDataViewer() {
     return (
         <>
             <div className="d-flex justify-content-between align-items-center mb-3">
-                <h3>{searchTerm.length === 0 ? "서버별" : searchTerm} Top 10 (총 {filteredAlliance.length.toLocaleString()}개)</h3>
+                <h3>{t(`TopwarAllianceDataViewer.label-title`)}</h3>
                 {/* 검색 입력창 추가 */}
                 <div style={{ width: '250px' }}>
                     <input 
                         type="text" 
                         className="form-control" 
-                        placeholder="서버 검색" 
+                        placeholder={t(`TopwarAllianceDataViewer.search-placeholder`)}
                         value={searchTerm}
                         onChange={(e) => {
                             const regex = /[0-9]*/;
@@ -42,7 +44,25 @@ export default function TopwarAllianceDataViewer() {
                 </div>
             </div>
 
-            <div className="row mt-2">
+            <div className="row mt-4">
+                <div className="col fw-bold text-primary">
+                    <div className="d-flex align-items-center border-bottom bg-white">
+                        <div style={{ width: 70 }}>
+                            {t(`TopwarAllianceDataViewer.data-rank`)}
+                        </div>
+                        <div style={{ width: 70 }} className="text-start">
+                            {t(`TopwarAllianceDataViewer.data-server`)}
+                        </div>
+                        <div className="text-truncate flex-grow-1">
+                            {t(`TopwarAllianceDataViewer.data-name`)}
+                        </div>
+                        <div style={{ width: 150 }} className="text-center">
+                            {t(`TopwarAllianceDataViewer.data-score`)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
                 <div className="col">
                     <Virtuoso
                         // useWindowScroll 사용 시 style의 height는 초기 렌더링 높이 역할을 하거나 
@@ -53,9 +73,12 @@ export default function TopwarAllianceDataViewer() {
                         itemContent={(index, alliance) => (
                             <div className="d-flex align-items-center border-bottom bg-white" 
                                  style={{ height: '35px', boxShadow: "0 0 0 0 lightgray" }}>
-                                <div style={{ width: 75 }}>
+                                <div style={{ width: 70 }}>
                                     {/* filteredAlliance를 기준으로 index 재계산 */}
                                     <span className="badge text-bg-primary">{alliance.rank}</span>
+                                </div>
+                                <div style={{ width: 75 }} className="text-start fw-bold">
+                                    {alliance.server}
                                 </div>
                                 <div className="text-truncate flex-grow-1">
                                     <strong>[{alliance.tag}] {alliance.name}</strong>
@@ -63,14 +86,11 @@ export default function TopwarAllianceDataViewer() {
                                 <div style={{ width: 150 }} className="text-end pe-3 numeric-cell fw-bold">
                                     {alliance.score.toLocaleString()}
                                 </div>
-                                <div style={{ width: 75 }} className="text-end pe-3 numeric-cell fw-bold">
-                                    {alliance.server}
-                                </div>
                             </div>
                         )}
                     />
                     {filteredAlliance.length === 0 && (
-                        <div className="text-center py-5 text-muted">검색 결과가 없습니다.</div>
+                        <div className="text-center py-5 text-muted">{t(`TopwarAllianceDataViewer.no-result`)}</div>
                     )}
                 </div>
             </div>
