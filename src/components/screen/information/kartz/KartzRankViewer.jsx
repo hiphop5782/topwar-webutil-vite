77293508ef05ed4521ae3c134af23752f82ea698
hiphop5782 <zdlsz@hanmail.net>
2026-02-26 23:31:18 +0900

    카르츠 뷰어 방식 변경

--- src/components/screen/information/kartz/KartzRankViewer.jsx
+++ src/components/screen/information/kartz/KartzRankViewer.jsx
@@ -2,6 +2,8 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
+import "./KartzData.css";
+
 const jsonModules = import.meta.glob('@src/assets/json/kartz/history/*.json');
 
 export default function KartzRankViewer(){
@@ -45,6 +47,24 @@ export default function KartzRankViewer(){
         return rankData !== null;
     }, [rankData]);
 
+    const [serverInput, setServerInput] = useState("");
+    const changeServerInput = useCallback((e) => {
+        const value = e.target.value;
+        // 빈 문자열이거나(전체 삭제 대응) 숫자만 포함된 경우에만 업데이트
+        if (value === "" || /^[0-9]+$/.test(value)) {
+            setServerInput(value);
+        }
+    }, []); // 의존성 배열을 비워 함수 재생성을 방지합니다.
+
+    const filteredUserRankData = useMemo(()=>{
+        if(serverInput === "") return userRankData;
+        return userRankData.filter(user=>user.server === parseInt(serverInput));
+    }, [userRankData, serverInput]);
+    const filteredAllianceRankData = useMemo(()=>{
+        if(serverInput === "") return allianceRankData;
+        return allianceRankData.filter(alliance=>alliance.server === parseInt(serverInput));
+    }, [allianceRankData, serverInput]);
+
     return (<>
         <label className="d-flex">
             <span className="d-flex align-items-center">회차</span>
@@ -60,31 +80,37 @@ export default function KartzRankViewer(){
             </button>
         </label>
 
-        {rankData !== null && (
+        {dataExist && (
+        <div className="row mt-2">
+            <div className="col d-flex">
+                <label className="col-form-label me-4">서버</label>
+                <input type="text" className="form-control w-auto" placeholder="e.g., 3223"
+                            value={serverInput} onChange={changeServerInput} inputMode="numeric"/>
+            </div>
+        </div>
+        )}
+
+        {dataExist && (
         <div className="row mt-4">
             <div className="col-md-6">
-                <h3>유저 순위</h3>
+                <h3>유저 순위 (총 {filteredUserRankData.length}명)</h3>
                 <div className="text-nowrap text-responsive">
-                    <table className="table table-striped">
+                    <table className="table table-striped table-rank">
                         <thead>
                             <tr>
-                                <th>순위</th>
-                                <th>서버</th>
+                                <th width={45}>순위</th>
+                                <th width={55}>서버</th>
                                 <th>유저명</th>
                                 <th className="text-end">라운드</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {userRankData.map((player, index)=>(
+                            {filteredUserRankData.map((player, index)=>(
                             <tr key={index}>
-                                <td>
-                                    <span className="badge text-bg-secondary">
-                                        {player.rank}
-                                    </span>
-                                </td>
-                                <td>{player.server}</td>
+                                <td>{player.rank}</td>
+                                <td className="numeric-cell">{player.server}</td>
                                 <td>{player.nickname}</td>
-                                <td className="text-end">{player.round}</td>
+                                <td className=" numeric-cell text-end">{player.round}</td>
                             </tr>
                             ))}
                         </tbody>
@@ -92,24 +118,24 @@ export default function KartzRankViewer(){
                 </div>
             </div>
             <div className="col-md-6">
-                <h3>동맹 순위</h3>
+                <h3>동맹 순위 (총 {filteredAllianceRankData.length}개)</h3>
                 <div className="text-nowrap table-responsive">
-                    <table className="table table-striped">
+                    <table className="table table-striped table-rank">
                         <thead>
                             <tr>
-                                <th>순위</th>
-                                <th>서버</th>
+                                <th width={45}>순위</th>
+                                <th width={55}>서버</th>
                                 <th>동맹명</th>
-                                <th className="text-end">점수</th>
+                                <th className="text-end" width={75}>점수</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {allianceRankData.map((alliance, index)=>(
+                            {filteredAllianceRankData.map((alliance, index)=>(
                             <tr key={index}>
                                 <td>{alliance.rank}</td>
-                                <td>{alliance.server}</td>
-                                <td>{alliance.name}</td>
-                                <td className="text-end">{alliance.score.toLocaleString()}</td>
+                                <td className="numeric-cell">{alliance.server}</td>
+                                <td className="text-truncate">[{alliance.tag}] {alliance.name}</td>
+                                <td className="text-end numeric-cell">{alliance.score.toLocaleString()}</td>
                             </tr>
                             ))}
                         </tbody>
