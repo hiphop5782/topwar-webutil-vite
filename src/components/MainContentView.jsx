22cf362c7dd532f26dffafba9230dcf620c68171
hiphop5782 <zdlsz@hanmail.net>
2026-06-22 22:57:37 +0900

    광고 줄바뀜 문제 해결

--- src/components/MainContentView.jsx
+++ src/components/MainContentView.jsx
@@ -60,7 +60,8 @@ export default function MainContentView() {
 
     return (
         <div className="row mb-5 pb-5">
-            <div className="col-md-2 d-none d-md-flex justify-content-center align-items-start" style={{minWidth:"160px"}}>
+            {/* <div className="col-md-2 d-none d-md-flex justify-content-center align-items-start" style={{minWidth:"160px"}}> */}
+            <div className="col-md-2 d-none d-md-flex justify-content-center align-items-start">
                 {/* <GoogleAdsVertical dataAdClient="ca-pub-5256661935690588" dataAdSlot="2606768455"/> */}
                 <KakaoAds id="DAN-2TYGu5OktHTg0aW6" width={160} height={600}/>
             </div>
@@ -154,7 +155,8 @@ export default function MainContentView() {
                     <Route path="*" element={<PangeNotFound />}></Route>
                 </Routes>
             </div>
-            <div className="col-md-2 d-none d-md-flex justify-content-center align-items-start" style={{minWidth:"160px"}}>
+            {/* <div className="col-md-2 d-none d-md-flex justify-content-center align-items-start" style={{minWidth:"160px"}}> */}
+            <div className="col-md-2 d-none d-md-flex justify-content-center align-items-start">
                 {/* <GoogleAdsVertical dataAdClient="ca-pub-5256661935690588" dataAdSlot="8253345796"/> */}
                 <KakaoAds id="DAN-WwP4DvEIbCS6Wv93"/>
             </div>
